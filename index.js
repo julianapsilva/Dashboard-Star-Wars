34290daef6a2447d9b3233b3fbc2d79696f6032c
julianapsilva <julianadepaula001@gmail.com>
2021-05-02 14:50:01 -0300

    dashboard finalizada

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ const navesContador = document.getElementById("naves");
 preencheContadores();
 preencherTabela();
 
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+async function drawChart() {
+  const response = await swapiGet("vehicles/");
+  const vehiclesArray = response.data.results;
+
+  const dataArray = [];
+  dataArray.push(["Veículos", "Passageiros"]);
+  vehiclesArray.forEach((vehicle) => {
+    dataArray.push([vehicle.name, Number(vehicle.passengers)]);
+  });
+
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    title: "Maiores veículos",
+    legend: "none",
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
+
 function preencheContadores() {
   Promise.all([
     swapiGet("people/"),
@@ -23,17 +50,14 @@ function preencheContadores() {
 async function preencherTabela() {
   const response = await swapiGet("films/");
   const tableData = response.data.results;
-  tableData.forEach( film => {
-    $('#filmsTable').append(`<tr>
+  tableData.forEach((film) => {
+    $("#filmsTable").append(`<tr>
     <td>${film.title}</td>
-    <td>${film.release_date}</td>
+    <td>${moment(film.release_date).format("DD/MM/YYYY")}</td>
     <td>${film.director}</td>
     <td>${film.episode_id}</td>
-    </tr>`)
-  })
-  
-  
-
+    </tr>`);
+  });
 }
 
 function swapiGet(param) {
